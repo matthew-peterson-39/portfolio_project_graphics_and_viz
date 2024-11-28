3fd47cf963b190a1fd966a3e98a16d208a828a15
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-11-28 10:41:35 -0500

    add vertexShaderSource

--- webgl.js
+++ webgl.js
@@ -5,3 +5,16 @@ if (!gl) {
   console.error("WebGL not supported");
   throw new Error("WebGL not supported");
 }
+
+// Add vertex and fragment shader source code
+const vertexShaderSource = `
+    attribute vec4 aVertexPosition;
+    attribute vec4 aVertexColor;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+    varying lowp vec4 vColor;
+    void main(void) {
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+        vColor = aVertexColor;
+    }
+`;
