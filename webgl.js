36f746545c84bf042fca9c23b65c36dde0e42a62
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-11-28 10:44:09 -0500

    add createShader function

--- webgl.js
+++ webgl.js
@@ -25,3 +25,17 @@ const fragmentShaderSource = `
         gl_FragColor = vColor;
     }
 `;
+
+// Add shader creation utility functions
+function createShader(gl, type, source) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      console.error("Shader compilation error:", gl.getShaderInfoLog(shader));
+      gl.deleteShader(shader);
+      return null;
+  }
+  return shader;
+}
